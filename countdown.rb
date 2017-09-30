d0dff3ed91ac3880d164094362e7d92fd519744b
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 01:18:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(number)
     "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(num_secs)
+    number -= 1
+  end
+    "HAPPY NEW YEAR!"
 end
